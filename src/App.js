b28b3654401f49c15aa191fcd6f0876ee6a5fb49
MarcosPMC1 <marcospmc@outlook.com>
2022-09-29 20:05:58 -0300

    responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import "./App.css";
 import { BsPerson } from "react-icons/bs";
-import { FaReact, FaGraduationCap } from "react-icons/fa";
-import { DiJavascript1 } from "react-icons/di";
-import { TbCertificate } from 'react-icons/tb'
+import { FaReact, FaGraduationCap, FaPhp } from "react-icons/fa";
+import {
+  DiJavascript1,
+  DiCss3Full,
+  DiPython,
+  DiJqueryLogo,
+} from "react-icons/di";
+import { TbCertificate } from "react-icons/tb";
+import { GrNode } from "react-icons/gr";
+import { AiOutlineHtml5 } from "react-icons/ai";
+
+// import PDF from './assets/Marcos_CardosoCV.pdf'
 
 function App() {
+  const onButtonClick = () => {
+    // using Java Script method to get PDF file
+    fetch('Marcos_CardosoCV.pdf').then(response => {
+        response.blob().then(blob => {
+            // Creating new object of PDF file
+            const fileURL = window.URL.createObjectURL(blob);
+            // Setting various property values
+            let alink = document.createElement('a');
+            alink.href = fileURL;
+            alink.download = 'MarcosCardosoCV.pdf';
+            alink.click();
+        })
+    })
+}
+
   return (
     <div className="App">
       <div className="card">
@@ -17,12 +41,53 @@ function App() {
               <h3 className="name">Marcos Paulo Muniz Cardoso</h3>
               <span>Estagiário CRG GESTÃO | Full-Stack </span>
             </div>
+            <div className="cv">
+              <button class="download-button" onClick={onButtonClick}> 
+                <div class="docs">
+                  <svg
+                    class="css-i6dzq1"
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    fill="none"
+                    stroke-width="2"
+                    stroke="currentColor"
+                    height="20"
+                    width="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+                    <polyline points="14 2 14 8 20 8"></polyline>
+                    <line y2="13" x2="8" y1="13" x1="16"></line>
+                    <line y2="17" x2="8" y1="17" x1="16"></line>
+                    <polyline points="10 9 9 9 8 9"></polyline>
+                  </svg>{" "}
+                  Curriculo
+                </div>
+                <div class="download">
+                  <svg
+                    class="css-i6dzq1"
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    fill="none"
+                    stroke-width="2"
+                    stroke="currentColor"
+                    height="24"
+                    width="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                    <polyline points="7 10 12 15 17 10"></polyline>
+                    <line y2="3" x2="12" y1="15" x1="12"></line>
+                  </svg>
+                </div>
+              </button>
+            </div>
           </div>
           <div className="container">
-            <h3>Formação</h3>
             <div className="formacao">
-            <div className="card--formacao">
-                <TbCertificate size={40} /> 
+              <h3>Formação</h3>
+              <div className="card--formacao">
+                <TbCertificate size={40} />
                 <div className="info">
                   <h5>Curso técnico</h5>
                   <label>UNINTER | Lógica de programação</label>
@@ -31,10 +96,13 @@ function App() {
               </div>
               <hr />
               <div className="card--formacao">
-              <TbCertificate size={40} /> 
+                <TbCertificate size={40} />
                 <div className="info">
                   <h5>Curso técnico</h5>
-                  <label>ETEC Polivalente Americana | Análise e Desenvolvimento de Sistemas</label>
+                  <label>
+                    ETEC Polivalente Americana | Análise e Desenvolvimento de
+                    Sistemas
+                  </label>
                   <small>2019 - 2021</small>
                 </div>
               </div>
@@ -43,7 +111,9 @@ function App() {
                 <FaGraduationCap size={40} />
                 <div className="info">
                   <h5>Ensino Superior</h5>
-                  <label>FATEC Americana | Análise e Desenvolvimento de Sistemas</label>
+                  <label>
+                    FATEC Americana | Análise e Desenvolvimento de Sistemas
+                  </label>
                   <small>2022 - 2024</small>
                 </div>
               </div>
@@ -60,7 +130,7 @@ function App() {
               <div className="card--language">
                 <FaReact size={32} />
                 <div className="card--language--header">
-                  <h3>React Native</h3>
+                  <h3>ReactNative</h3>
                   <h3>Mobile</h3>
                 </div>
               </div>
@@ -72,45 +142,45 @@ function App() {
                 </div>
               </div>
               <div className="card--language">
-                <DiJavascript1 size={32} />
+                <AiOutlineHtml5 size={32} />
                 <div className="card--language--header">
                   <h3>HTML</h3>
                   <h3>WEB</h3>
                 </div>
               </div>
               <div className="card--language">
-                <DiJavascript1 size={32} />
+                <DiCss3Full size={32} />
                 <div className="card--language--header">
                   <h3>CSS</h3>
                   <h3>WEB</h3>
                 </div>
               </div>
               <div className="card--language">
-                <DiJavascript1 size={32} />
+                <GrNode size={32} />
                 <div className="card--language--header">
                   <h3>Node</h3>
                   <h3>WEB</h3>
                 </div>
               </div>
               <div className="card--language">
-                <DiJavascript1 size={32} />
+                <FaPhp size={32} />
                 <div className="card--language--header">
                   <h3>PHP</h3>
                   <h3>WEB</h3>
                 </div>
               </div>
               <div className="card--language">
-                <DiJavascript1 size={32} />
+                <DiPython size={32} />
                 <div className="card--language--header">
                   <h3>Python</h3>
                   <h3>WEB</h3>
                 </div>
               </div>
               <div className="card--language">
-                <DiJavascript1 size={32} />
+                <DiJqueryLogo size={32} />
                 <div className="card--language--header">
-                  <h3>C</h3>
-                  <h3>Desktop</h3>
+                  <h3>Jquery</h3>
+                  <h3>WEB</h3>
                 </div>
               </div>
             </div>
